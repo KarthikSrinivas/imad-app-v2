6511638000964756ac7ded10c21836de4441eddb
KarthikSrinivas <1416047@saec.ac.in>
2017-02-22 21:22:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/article',function(req,res){
 });
 
 app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+  res.sendFile(path.join(__dirname, 'ui','server.js'));
 });
 
 
