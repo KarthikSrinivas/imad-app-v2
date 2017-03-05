830bac6d37cf1eec55d1b6bca7b6a35cd107b2e5
KarthikSrinivas <1416047@saec.ac.in>
2017-03-05 20:57:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/articletwo',function(req,res){
     res.sendFile(path.join(__dirname,'ui','article-two.html')); 
 });
 app.get('/articlethree',function(req,res){
-    res.sendFile(path.join(__dirname,'ui','article-one.html'));
+    res.sendFile(path.join(__dirname,'ui','article-three.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
