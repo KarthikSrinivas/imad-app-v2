e8fa7da991fdb3c3e6271d9e2d54dc2c19e326ec
KarthikSrinivas <1416047@saec.ac.in>
2017-03-24 18:32:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,5 +2,5 @@ var button=document.getElementById(1);
 button.onclick = function()
 {
     couter=counter+1;
-    document.getElementById("th").innerHTML=counter;
-}
+    document.getElementById("th").innerHTML=counter.toString();
+};
