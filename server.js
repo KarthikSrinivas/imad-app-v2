8b95c78cfe91dfced8512f081463f80ff0498863
KarthikSrinivas <1416047@saec.ac.in>
2017-04-18 14:08:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,53 +14,7 @@ var config={
 
 var app = express();
 app.use(morgan('combined'));
-var articles={
-    
 
-    "article-one" :{
-    title:"pskarthikeyan1",
-    con:`<p>
-            serving the client 
-        </p>`
-    
-   },
-   "article-two":{
-       title:"PSKARTHIKEYAN",
-       con:`<p>
-       SERVING THE CLIENT</p>   
-        `},
-       "articlethree":{
-           title:"KARTHIK SRINIVAS",
-           con:`<p>
-           PRODUCT BASED
-           </p>`
-       }
-    
-};
-function createTemplate(data){
-    var title=data.title;
-    var con=data.con;
-
-var htmlTemplate=`
-<html>
-    <head>
-        <title>
-             ${title}
-        </title>
-         <link href="/ui/style.css" type="stylesheet"/>
-    </head>
-<body>
-    
-        <a href="/"> click here </a><br/>
-        <h1 class="container">${con}</h1>
-         <p>
-            serving the client 
-        </p>
-    
-</body>
-</html>`;
-return htmlTemplate;
-}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -89,45 +43,3 @@ app.get('/counter',function(req,res)
     res.send(counter.toString());
 });
 var pool=new Pool(config);
-app.get('/test-db',function(req,res){
-   pool.query("SELECT * FROM  test",function (err,result){
-      if(err)
-      {
-          res.status(500).send(err.toString());
-      }
-      else
-      {
-          res.send(JSON.stringify(result));
-      }
-   });
-    
-});
-app.get('/:articleName',function(req,res){
-    var articleName=req.params.articleName;
-res.send(createTemplate(articles[articleName]));
-});
-
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-function hash(input,salt)
-{ 
-    var gas=crypto.pbkdf2Sync(input, salt, 100000, 512, 'sha512');
-    return ["pbdkdf","10000",salt,hashed.toString('hex')].join('$');
-    
-}
-app.get('/hash/:name',function(req,res)
-{
-   var hashedstring=hash(req.params.name,"this is a salt value");
-   res.send("hashedstring");
-});
-
-
-var port = 8080; // Use 8080 for local development because you might already have apache running on 80
-app.listen(8080, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
-});
-app.use(bodyParser.urlencoded({
-extended: true
-}));
-app.use(bodyParser.json());
